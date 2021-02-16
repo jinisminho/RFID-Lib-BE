69755172385357f86620e28e9272f0ade51a74f9
Tram Phan <hoang.tram2908@gmail.com>
2021-02-16 09:21:40 +0700

    fix account services
    create lib, email template, valid msg

--- library-BE/src/main/java/capstone/library/services/MailService.java
+++ library-BE/src/main/java/capstone/library/services/MailService.java
@@ -3,6 +3,7 @@ package capstone.library.services;
 import capstone.library.dtos.email.EmailCheckOutBookDto;
 import capstone.library.dtos.email.EmailReturnBookDto;
 import capstone.library.dtos.response.CheckoutResponseDto;
+import capstone.library.dtos.response.ReturnBookResponseDto;
 
 import java.util.List;
 
@@ -10,7 +11,7 @@ public interface MailService {
 
     void sendCheckoutMail (String patronEmail, CheckoutResponseDto request);
 
-    void sendReturnMail (List<EmailReturnBookDto> books);
+    void sendReturnMail (List<ReturnBookResponseDto> books);
 
     void sendRemindOverdueBook();
 
