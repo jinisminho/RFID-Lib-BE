9bf630e11f8932c4768a487b2922bb678d7eb471
Khang Nguyen <khang090999@gmail.com>
2021-02-22 21:48:54 +0700

    [khangndn] scheduling

--- library-BE/src/main/java/capstone/library/scheduling/ScheduledCheckOverdue.java
+++ library-BE/src/main/java/capstone/library/scheduling/ScheduledCheckOverdue.java
@@ -1,12 +1,22 @@
 package capstone.library.scheduling;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.scheduling.annotation.Scheduled;
+import capstone.library.services.MailService;
 
 @Component("scheduledCheckOverdue")
 public class ScheduledCheckOverdue {
+    @Autowired
+    private MailService mailService;
+
     @Scheduled(cron = "${cron.expression}")
-    public void scheduleTaskUsingExternalizedCronExpression() {
-        System.out.println("schedule tasks using externalized cron expressions - " + System.currentTimeMillis() / 1000);
+    public void scheduleCheckOverdue() {
+        mailService.sendRemindOverdueBook();
+    }
+
+    @Scheduled(cron = "${cron.expression}")
+    public void scheduleNotifyWishlist() {
+        mailService.sendNotifyWishlistAvailable();
     }
 }
