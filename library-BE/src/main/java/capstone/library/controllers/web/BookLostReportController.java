172e6da65cb4df2b3f2e5d662b2949fcb8de6d62
Hoang Pham <hoangpham1.4.99@gmail.com>
2021-02-26 18:28:25 +0700

    resolve conflict with dev

--- library-BE/src/main/java/capstone/library/controllers/web/BookLostReportController.java
+++ library-BE/src/main/java/capstone/library/controllers/web/BookLostReportController.java
@@ -1,18 +1,17 @@
 package capstone.library.controllers.web;
 
 import capstone.library.dtos.response.BookLostResponse;
+import capstone.library.dtos.response.LostBookFineResponseDto;
 import capstone.library.services.BookLostReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.constraints.NotNull;
-import capstone.library.dtos.response.LostBookFineResponseDto;
+import java.time.LocalDateTime;
+
 import static capstone.library.util.constants.ConstantUtil.DATE_TIME_PATTERN;
 @RestController
 @RequestMapping("/lost")
--- library-BE/src/main/java/capstone/library/services/impl/BookLostReportServiceImpl.java
+++ library-BE/src/main/java/capstone/library/services/impl/BookLostReportServiceImpl.java
@@ -1,17 +1,24 @@
 package capstone.library.services.impl;
 
 import capstone.library.dtos.common.BookBorrowingDto;
+import capstone.library.dtos.response.BookLostResponse;
 import capstone.library.dtos.response.LostBookFineResponseDto;
 import capstone.library.entities.BookBorrowing;
+import capstone.library.entities.BookLostReport;
 import capstone.library.entities.FeePolicy;
+import capstone.library.exceptions.MissingInputException;
 import capstone.library.exceptions.ResourceNotFoundException;
 import capstone.library.repositories.BookBorrowingRepository;
+import capstone.library.repositories.BookLostReportRepository;
 import capstone.library.repositories.FeePolicyRepository;
 import capstone.library.services.BookLostReportService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -21,6 +28,8 @@ public class BookLostReportServiceImpl implements BookLostReportService {
     BookBorrowingRepository bookBorrowingRepository;
     @Autowired
     FeePolicyRepository feePolicyRepository;
+    @Autowired
+    BookLostReportRepository bookLostReportRepository;
     @Autowired
     ObjectMapper objectMapper;
 
@@ -38,39 +47,6 @@ public class BookLostReportServiceImpl implements BookLostReportService {
         return response;
     }
 
-    private double calculateLostFineInMarket(BookBorrowing bookBorrowing) {
-        //Get the latest fee policy. latest fee policy is the first item in the list
-        List<FeePolicy> feePolicies = feePolicyRepository.findAllByOrderByCreatedAtDesc();
-        return bookBorrowing.getBookCopy().getPrice() + feePolicies.get(0).getDocumentProcessing_Fee();
-    }
-
-    private double calculateLostFineNotInMarket(BookBorrowing bookBorrowing) {
-        //Get the latest fee policy. latest fee policy is the first item in the list
-        List<FeePolicy> feePolicies = feePolicyRepository.findAllByOrderByCreatedAtDesc();
-        return bookBorrowing.getBookCopy().getPrice() * feePolicies.get(0).getMissingDocMultiplier();
-    }
-}
-
-package capstone.library.services.impl;
-
-import capstone.library.dtos.response.BookLostResponse;
-import capstone.library.entities.BookLostReport;
-import capstone.library.exceptions.MissingInputException;
-import capstone.library.repositories.BookLostReportRepository;
-import capstone.library.services.BookLostReportService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-
-@Service
-public class BookLostReportServiceImpl implements BookLostReportService {
-
-    @Autowired
-    private BookLostReportRepository bookLostReportRepo;
-
     @Override
     public Page<BookLostResponse> findBookLostInPeriod(LocalDateTime startDate,
                                                        LocalDateTime endDate,
@@ -83,13 +59,13 @@ public class BookLostReportServiceImpl implements BookLostReportService {
             startDate = endDate;
             endDate = tmp;
         }
-        return bookLostReportRepo
+        return bookLostReportRepository
                 .findByLostAtBetweenOrderByLostAtDesc(startDate, endDate, pageable)
                 .map(this::mapBookLostEntityToBookLostDto);
     }
 
-    private BookLostResponse mapBookLostEntityToBookLostDto(BookLostReport entity){
-        BookLostResponse dto = new  BookLostResponse();
+    private BookLostResponse mapBookLostEntityToBookLostDto(BookLostReport entity) {
+        BookLostResponse dto = new BookLostResponse();
         dto.setId(entity.getId());
         dto.setLostAt(entity.getLostAt());
         dto.setBorrowedAt(entity.getBookBorrowing().getBorrowing().getBorrowedAt());
@@ -102,5 +78,17 @@ public class BookLostReportServiceImpl implements BookLostReportService {
         dto.setEdition(entity.getBookBorrowing().getBookCopy().getBook().getEdition());
         dto.setISBN(entity.getBookBorrowing().getBookCopy().getBook().getIsbn());
         return dto;
+    }
+
+    private double calculateLostFineInMarket(BookBorrowing bookBorrowing) {
+        //Get the latest fee policy. latest fee policy is the first item in the list
+        List<FeePolicy> feePolicies = feePolicyRepository.findAllByOrderByCreatedAtDesc();
+        return bookBorrowing.getBookCopy().getPrice() + feePolicies.get(0).getDocumentProcessing_Fee();
+    }
+
+    private double calculateLostFineNotInMarket(BookBorrowing bookBorrowing) {
+        //Get the latest fee policy. latest fee policy is the first item in the list
+        List<FeePolicy> feePolicies = feePolicyRepository.findAllByOrderByCreatedAtDesc();
+        return bookBorrowing.getBookCopy().getPrice() * feePolicies.get(0).getMissingDocMultiplier();
     }
 }
