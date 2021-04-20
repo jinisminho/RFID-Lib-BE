2b322070f7ee6801c55174e852a26929201f4f7e
KienNT <knguyen.05.07.97@gmail.com>
2021-04-21 01:25:56 +0700

    [BE] API get borrow, filter by borrowed date

--- library-BE/src/main/java/capstone/library/mappers/BookCopyMapper.java
+++ library-BE/src/main/java/capstone/library/mappers/BookCopyMapper.java
@@ -2,10 +2,12 @@ package capstone.library.mappers;
 
 import capstone.library.dtos.common.ProfileDto;
 import capstone.library.dtos.response.BookCopyResDto;
+import capstone.library.dtos.response.CopyResponseDto;
 import capstone.library.entities.BookCopy;
 import capstone.library.entities.Profile;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring", uses = BookMapper.class)
 public interface BookCopyMapper {
@@ -20,5 +22,15 @@ public interface BookCopyMapper {
 //    BookCopyDto toDto(BookCopy entity);
 
     BookCopyResDto toResDto(BookCopy entity);
+
+
+    @Mappings({
+            @Mapping(target = "borrowing.bookBorrowings", ignore = true),
+            @Mapping(target = "copyType", source = "bookCopyType.name"),
+            @Mapping(target = "book.barcode", source = "barcode"),
+            @Mapping(target = "book.rfid", source = "rfid"),
+            @Mapping(target = "book", source = "book"),
+    })
+    CopyResponseDto toResAltDto(BookCopy entity);
 
 }
